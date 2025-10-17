154e34988843c35a0f8578ea9c318b865d056039
Евтухов Дмитрий <justarcane@vk.com>
2025-10-17 17:40:09 +0300

    Added project

---
+++ Degree/Degree/Degree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degree
+{
+    public class Degree
+    {
+        
+        public static double GetDegree(double baseint, double total)
+        {
+            return Math.Log(total) / Math.Log(baseint);
+        }
+
+    }
+}
---
+++ Degree/Degree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Degree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"степень: {Degree.GetDegree(2, 29)}");
+        }
+    }
+}
